fb009cec03f0542c7767330432c97c08fab17895
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-06-03 17:15:17 +0300

    1

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,24 @@
 package com.example.servlet;
+import com.example.User;
+import com.example.Warehouse;
 
-public class AddUserServlet {
-    //write your code here!
+import  javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName= req.getParameter("firstName");
+        String lastName= req.getParameter("lastName");
+        User user=new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        Warehouse.getInstance().addUser(user);
+        req.setAttribute("user",firstName+" "+lastName);
+        req.getRequestDispatcher("/add.jsp").forward(req,resp);
+    }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,18 @@
 package com.example.servlet;
+import com.example.User;
+import com.example.Warehouse;
 
-public class GetUsersServlet {
-    //write your code here!
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user= (User) Warehouse.getInstance().getUsers();
+        request.setAttribute("users", user);
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
+    }
 }
